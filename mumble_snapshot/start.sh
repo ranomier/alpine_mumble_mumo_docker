fde6337c75082b01fc88f8e52620389baeaf282c
Ranomier <ranomier@fragomat.net>
2018-04-04 23:09:42 +0200

    permission problem

--- mumble_snapshot/start.sh
+++ mumble_snapshot/start.sh
@@ -1,14 +1,22 @@
 #!/bin/ash
-printf "using start script\n"
+printf "######################\n"
+printf "# using start script #\n"
+printf "######################\n"
 mkdir -p "${HOME}/data"
+chown -R murmur "${HOME}/data"
+chmod -R u+rw "${HOME}/data"
 murmur_loc="${HOME}/data/murmur.ini"
 if [ -e "${murmur_loc}" ]; then
-    mv "${HOME}/murmur.ini" "orignal_${murmur_loc}"
+    mv "${HOME}/murmur.ini" "${murmur_loc}.original"
 else
     mv "${HOME}/murmur.ini" "${murmur_loc}"
     sed -i -e "s|database=.*|database=${HOME}/data/murmur.sqlite" "${murmur_loc}" 
 fi
 printf "$(</dev/urandom tr -dc _A-Z-a-z-0-9 | head -c16)" > "${HOME}/data/icepassword"
 printf "$(hostname)" > "${HOME}/data/murmur_hostname"
+
+printf "####################\n"
+printf "# end start script #\n"
+printf "####################\n"
 
 ${*}
