3a79d45d86f45f272b71c493d96aa0f182dfa242
Ranomier <ranomier@fragomat.net>
2018-04-13 15:26:30 +0200

    created share volume for icepassword

--- mumble_snapshot/start.sh
+++ mumble_snapshot/start.sh
@@ -12,8 +12,8 @@ else
     mv "${HOME}/murmur.ini" "${murmur_loc}"
     sed -i -e "s|database=.*|database=${HOME}/data/murmur.sqlite" "${murmur_loc}" 
 fi
-printf "$(</dev/urandom tr -dc _A-Z-a-z-0-9 | head -c16)" > "${HOME}/data/icepassword"
-printf "$(hostname)" > "${HOME}/data/murmur_hostname"
+printf "$(</dev/urandom tr -dc _A-Z-a-z-0-9 | head -c16)" > "${HOME}/share/icepassword"
+printf "$(hostname)" > "${HOME}/share/murmur_hostname"
 
 printf "####################\n"
 printf "# end start script #\n"
